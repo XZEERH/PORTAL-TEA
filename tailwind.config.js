f8f326a0d14ae44a5102d4c378d7b67ba0d481e1
XZEERH <razzyr408@gmail.com>
2026-04-21 22:46:57 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content:[
+    "./index.html",
+    "./src/**/*.{js,jsx}", // Membaca semua file JSX di folder src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins:
