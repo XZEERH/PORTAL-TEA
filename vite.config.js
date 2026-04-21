253c0674081eaea315c7f369ad505c39fe782bd2
XZEERH <razzyr408@gmail.com>
2026-04-22 04:21:28 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-// https://vitejs.dev/config/
+// Cara modern dapet __dirname di file .js (ESM)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
-  plugins:[react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // Arahkan ke folder src biar rapi
+    },
+  },
   server: {
-    host: true, // Membuka akses jaringan lokal (Penting untuk Termux/HP)
-    port: 5173,
-    strictPort: true
+    // Biar enteng di Termux
+    host: true,
+    port: 3000,
+    hmr: process.env.DISABLE_HMR !== 'true',
+  },
+  build: {
+    outDir: 'dist',
   }
-})
+});
